fed618aab3f61a5f19051751f951698446bbe002
Mctronco <fer_castellanos94@hotmail.com>
2025-06-29 22:57:57 -0600

    BreackOut
    Se creo el proyecto BreackOut

---
+++ BreackOutJuego/Assets/_Scripts/Bloque_Random.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Random : Bloque
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numeroRandom = 0;
+        numeroRandom = Random.Range(0, 7);
+        resistencia = numeroRandom;
+        Debug.Log(resistencia);
+    }
+
+
+}
